108ecba69f920123720d23d8cf6211432bb83f3d
Cesar <cesarnsingi@hotmail.com>
2025-03-13 23:07:29 +0200

    print round math log

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -4,3 +4,4 @@ lower = int(input("Enter the lower bound : "))
 upper = int(input("Enter the upper bound : "))
 
 x = random.randint(lower,upper)
+print("You have got",round(math.log(upper-lower+1,2)), "chances to guess.")
